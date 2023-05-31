bbe919e6649b0c0fed71e576a5693a0ea8276ee3
trinhthuc2t <thuctrinh.2t@gmail.com>
2023-05-31 14:21:15 +0700

    fix

--- case.js
+++ case.js
@@ -1,9 +1,76 @@
-let arr = [];
 
-function save() {
-    //console.log('ok')
+
+// function hideAll(){
+//     document.getElementById('display').innerHTML = ''
+// }
+
+
+function checkPass() {
+    let use = document.getElementById('use').value;
+    let pass = document.getElementById('pass').value;
+    if (use === 'admin' && pass === '123456') {
+        showDiv();
+        document.getElementById('checkpass').innerHTML = 'admin';
+        document.getElementById('form-container').style.flexDirection = 'column';
+
+       // document.getElementById('hideRanh').innerHTML = '';
+
+    } else {
+        document.getElementById('use').value = ''
+        document.getElementById('pass').value = ''
+        alert('Sai thông tin tài khoản');
+    }
+}
+
+
+
+
+
+function showDiv() {
+    let login = document.getElementById('display');
+     login.classList.add('display');
+}
+
+
+
+
+
+
+class Student {
+    name;
+    id;
+    point;
+    email;
+    phone;
+    address;
+    gender;
+
+    constructor(name, mssv, point, email,phone, address, gender) {
+        this.name = name;
+        this.id = mssv;
+        this.point = point;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.gender = gender;
+    }
+}
+
+
+
+
+let thuy = new Student('Tạ Thị Thùy', 'DTN082746',7.5 , 'tathuy@gmail.com', '0948292241', 'Hà Nội', 'Nữ');
+let thai = new Student('Hoàng Văn Thái', 'DTN082749',8.0, 'thai@gmail.com', '0990202731', 'Hà Nội', 'Nam');
+let arr = [thuy, thai];
+
+
+
+
+
+function add() {
     let fullName = document.getElementById('fullname').value;
     let mssv = document.getElementById('id').value;
+    let point = document.getElementById('point').value;
     let email = document.getElementById('email').value;
     let phone = document.getElementById('phonenumber').value;
     let address = document.getElementById('address').value;
@@ -16,79 +83,20 @@ function save() {
         gender = document.getElementById('3').value;
     }
 
-    class Student {
-        name;
-        id;
-        email;
-        phone;
-        address;
-        gender;
+    let list = new Student(fullName ,mssv ,point , email, phone, address, gender);
 
-        constructor(name, mssv, email, phone, address, gender) {
-            this.name = name;
-            this.id = mssv;
-            this.email = email;
-            this.phone = phone;
-            this.address = address;
-            this.gender = gender;
-        }
-
-        getName() {
-            return this.name;
-        }
-
-        getId() {
-            return this.id;
-        }
-
-        getEmail() {
-            return this.email;
-        }
-
-        getPhone() {
-            return this.phone;
-        }
-
-        getAddress() {
-            return this.address;
-        }
-
-        getGender() {
-            return this.gender
-
-        }
-
-        setName(newName) {
-            this.name = newName;
-        }
-
-        setId(newMssv) {
-            this.id = newMssv;
-        }
-
-        setEmail(newEmail) {
-            this.email = newEmail;
-        }
-
-        setPhone(newPhone) {
-            this.phone = newPhone;
-        }
-
-        setAddress(newAddress) {
-            this.address = newAddress;
-        }
-
-        setGander(newGender) {
-            this.gender = newGender
-
-        }
-    }
-
-    let list = new Student(fullName, mssv, email, phone, address, gender);
     arr.push(list);
-    console.log(arr);
+
     disPlay(arr);
+
 }
+
+
+
+
+
+console.log(arr);
+
 
 function disPlay(arr) {
     let table = `
@@ -97,13 +105,14 @@ function disPlay(arr) {
     for (let i = 0; i < arr.length; i++) {
         let listStudent = `
             <tr>
-                <td>${i+2}</td>
-                <td>${arr[i].getName()}</td>
-                <td>${arr[i].getId()}</td>
-                <td>${arr[i].getEmail()}</td>
-                <td>${arr[i].getPhone()}</td>
-                <td>${arr[i].getAddress()}</td>
-                <td>${arr[i].getGender()}</td>
+                <td>${i + 1}</td>
+                <td>${arr[i].name}</td>
+                <td>${arr[i].id}</td>
+                <td>${arr[i].point}</td>
+                <td>${arr[i].email}</td>
+                <td>${arr[i].phone}</td>
+                <td>${arr[i].address}</td>
+                <td>${arr[i].gender}</td>
                 <td><button onclick="deleteStudent(${i})" style="width: 50px">Delete 
                 <button onclick="editStuden(${i})" style="width: 50px">Edit</button></td>
             </tr>
@@ -115,38 +124,50 @@ function disPlay(arr) {
     document.getElementById('list').innerHTML = table;
     document.getElementById('fullname').value = '';
     document.getElementById('id').value = '';
+    document.getElementById('point').value = '';
     document.getElementById('email').value = '';
     document.getElementById('phonenumber').value = '';
     document.getElementById('address').value = '';
-    document.getElementById('1').checked = false
-    document.getElementById('2').checked = false
-    document.getElementById('3').checked = false
+    document.getElementById('1').checked = false;
+    document.getElementById('2').checked = false;
+    document.getElementById('3').checked = false;
+    arrPoint(arr);
 
 }
+
+
 
 
 
 function deleteStudent(index) {
- arr.splice(index,1);
- disPlay(arr)
+    arr.splice(index, 1);
+    disPlay(arr)
 }
+
+
+
+
+
 
 
 function editStuden(i) {
     let Student = arr[i];
-    document.getElementById('fullname').value = Student.getName() ;
-    document.getElementById('id').value = Student.getId();
-    document.getElementById('email').value = Student.getEmail();
-    document.getElementById('phonenumber').value = Student.getPhone();
-    document.getElementById('address').value = Student.getAddress();
+    document.getElementById('fullname').value = Student.name;
+    document.getElementById('id').value = Student.id;
+    document.getElementById('point').value = Student.point;
+    document.getElementById('email').value = Student.email;
+    document.getElementById('phonenumber').value = Student.phone;
+    document.getElementById('address').value = Student.address;
     document.getElementById('edit').innerHTML = `
-    <button onclick="edit(${i})"style="background-color: yellow">Lưu lại
+    <button onclick="edit(${i})" style="background-color: yellow">Lưu lại
     `;
 
 }
-function edit(index){
+
+function edit(index) {
     let newName = document.getElementById('fullname').value;
     let newMssv = document.getElementById('id').value;
+    let newPoint = document.getElementById('point').value;
     let newEmail = document.getElementById('email').value;
     let newPhone = document.getElementById('phonenumber').value;
     let newAddress = document.getElementById('address').value;
@@ -158,18 +179,46 @@ function edit(index){
     } else if (document.getElementById('3').checked) {
         newGender = document.getElementById('3').value;
     }
-    arr[index].setName(newName);
-    arr[index].setId(newMssv);
-    arr[index].setPhone(newPhone);
-    arr[index].setEmail(newEmail);
-    arr[index].setAddress(newAddress);
-    arr[index].setGander(newGender)
+    arr[index].name = newName;
+    arr[index].id = newMssv;
+    arr[index].point = newPoint;
+    arr[index].email = newEmail;
+    arr[index].phone = newPhone;
+    arr[index].address = newAddress;
+    arr[index].gender = newGender;
     disPlay(arr)
     document.getElementById('edit').innerHTML = ``
+    disPlay(arr)
 }
 
 
-console.log(arr);
+function search() {
+    let searchArr = []
+    let search = document.getElementById('search').value
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name.includes(search)) {
+            searchArr.push(arr[i]);
+        }
+    }
+    disPlay(searchArr);
+}
+disPlay(arr);
 
 
 
+
+function arrPoint(arr1){
+    let arrPoint2 = []
+    for (let i = 0; i < arr1.length; i++) {
+        arrPoint2.push(arr1[i]);
+    }
+    arrPoint2.sort((st1, st2) => st2.point - st1.point);
+    console.log(arrPoint2)
+    for (let i = 0; i < arrPoint2.length; i++){
+        if (i===5){
+            break
+        }
+    document.getElementById(`name${i+1}`).innerHTML = arrPoint2[i].name;
+    document.getElementById(`point${i+1}`).innerHTML = arrPoint2[i].point;
+    }
+}
